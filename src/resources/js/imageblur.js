95d3eff25590645cc3ca258b5e391aeba9e52c41
nikoci <arijan@nikoci.se>
2023-01-20 22:41:31 +0100

    fixed boicblur

--- src/resources/js/imageblur.js
+++ src/resources/js/imageblur.js
@@ -3,22 +3,52 @@ document.onload = (e) => {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('img.blur-boic')
+    const containers = document.querySelectorAll('.blur-boic')
 
-    images.forEach(img => {
-        img.style.position = 'relative'
-        img.style.zIndex = '2'
+    containers.forEach(container => {
+        var fgimg, bgimg, imgurl;
 
-        var boic = document.createElement('img')
+        container.style.position = 'relative'
 
-        boic.setAttribute('src', img.getAttribute('src'))
-        boic.style.position = 'absolute'
-        boic.style.top = '0'
-        boic.style.left = '0'
-        boic.style.transform = 'scale(1.05)'
-        boic.style.zIndex = '1'
-        boic.style.filter = 'blur(30px)'
+        imgurl = container.getAttribute('data-src')
 
-        img.parentElement.appendChild(boic)
+        bgimg = document.createElement('img')
+        bgimg.setAttribute('src', imgurl)
+        bgimg.style.position = 'absolute'
+        bgimg.style.top = '0'
+        bgimg.style.left = '0'
+        bgimg.style.width = 'inherit'
+        bgimg.style.height = 'inherit'
+        bgimg.style.transform = 'scale(1.05)'
+        bgimg.style.zIndex = '1'
+        bgimg.style.filter = 'blur(30px)'
+
+        fgimg = document.createElement('div')
+        fgimg.style.zIndex = '2'
+        fgimg.style.position = 'relative'
+        fgimg.style.backgroundImage = `url(${imgurl})`
+        fgimg.style.backgroundSize = 'cover'
+        fgimg.style.backgroundPosition = 'center'
+        fgimg.style.backgroundRepeat = 'no-repeat'
+        fgimg.style.borderRadius = '8px'
+        fgimg.style.width = 'inherit'
+        fgimg.style.height = 'inherit'
+        
+        container.appendChild(fgimg)
+        container.appendChild(bgimg)
     })
 })
+
+function getStyle(oElm, strCssRule){
+    var strValue = "";
+    if(document.defaultView && document.defaultView.getComputedStyle){
+        strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
+    }
+    else if(oElm.currentStyle){
+        strCssRule = strCssRule.replace(/\-(\w)/g, function (strMatch, p1){
+            return p1.toUpperCase();
+        });
+        strValue = oElm.currentStyle[strCssRule];
+    }
+    return strValue;
+}
